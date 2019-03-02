cbee29370dd4c51aecf7bf4b234db55ae4a42ac6
Volha Kazlovich <eexmellie@gmail.com>
2019-03-02 14:39:01 +0300

    feat: add implementation for getZerosCount()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  const basePrimeFactors = findPrimeFactors(base);
+  const powersOfFactors = [];
+
+  for (const [prime, amount] of basePrimeFactors.entries()) {
+    let powerOfPrime = 0;
+    for (let i = 1, pow = Math.pow(prime, i); pow <= number; i++, pow = Math.pow(prime, i)) {
+      powerOfPrime = powerOfPrime + Math.floor(number / pow);
+    }
+    powersOfFactors.push( Math.floor(powerOfPrime / amount) );
+  }
+  return Math.min(...powersOfFactors);
 }
+
+  function findPrimeFactors(number) {
+    const primeFactors = new Map();
+
+    // choosing starting value for the loop
+    // depending on wether number is even or odd
+    let i = number % 2 === 0 ? 2 : 3;
+    for (i; i <= number; i++) {
+      while(number % i === 0) {
+        primeFactors.set(i, primeFactors.get(i) + 1 || 1);
+        number = number / i;
+      }
+    }
+    return primeFactors;
+  }
